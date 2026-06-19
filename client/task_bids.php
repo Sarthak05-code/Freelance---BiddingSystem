6c93d59bc1f40687ebdb14729b169b126cb01fd0
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-19 18:17:36 +0545

    Fix formatting in task_bids.php

--- client/task_bids.php
+++ client/task_bids.php
@@ -178,6 +178,6 @@ require_once '../includes/header.php';
 
     </div>
 </div>
-                    <!-- Tester done and working.  -->
+                    <!-- Tester done and working. -->
 
 <?php require_once '../includes/footer.php'; ?>
